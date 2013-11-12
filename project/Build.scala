136bab01630b6a4cb4fd6c206074bfbe70f091b4
Brandon Arp <barp@groupon.com>
2013-11-12 12:48:18 -0800

    add metadata to build

--- project/Build.scala
+++ project/Build.scala
@@ -17,6 +17,35 @@ object ApplicationBuild extends Build {
   val main = play.Project(appName, appVersion, appDependencies).settings(
     javacOptions ++= Seq("-Xlint:deprecation", "-Xlint:unchecked"),
 
-    organization := "com.arpnetworking"
+    name := "arpnet-standard",
+    description := "Play module to setup a base set of standard monitoring and performance logging",
+    organization := "com.arpnetworking",
+    resolvers += "Local Maven" at Path.userHome.asFile.toURI.toURL + ".m2/repository",
+
+    pomIncludeRepository := { _ => false },
+    publishMavenStyle := true,
+    publishTo <<= version {
+      (v: String) =>
+        val nexus = "https://oss.sonatype.org/"
+        if (v.trim.endsWith("SNAPSHOT"))
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    },
+
+    licenses := Seq("MIT" -> url("http://opensource.org/licenses/MIT")),
+    homepage := Some (url("https://github.com/BrandonArp/arpnet-standard")),
+    pomExtra := (
+        <scm>
+          <url>git@github.com:BrandonArp/arpnet-standard.git</url>
+          <connection>scm:git:git@github.com:BrandonArp/arpnet-standard.git</connection>
+        </scm>
+        <developers>
+          <developer>
+            <id>barp</id>
+            <name>Brandon Arp</name>
+            <email>brandonarp@gmail.com</email>
+          </developer>
+        </developers>)
   )
 }
